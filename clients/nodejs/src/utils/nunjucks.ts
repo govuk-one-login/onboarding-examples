a6d3c0062db56fc8874205196df8bc1836c29a71
philf999 <112003420+philf999@users.noreply.github.com>
2025-02-04 12:48:26 +0000

    Change nunjucks view to hide veruify button when identity not enabled

--- clients/nodejs/src/utils/nunjucks.ts
+++ clients/nodejs/src/utils/nunjucks.ts
@@ -1,14 +1,13 @@
 import nunjucks from "nunjucks";
 import { Application } from "express";
-import { getNodeEnv } from "./config.js";
 
 export function setupNunjucks(app: Application, viewsPath: string): void {
-  const isDevelopment = getNodeEnv() !== "production";
+
   const configureOptions: nunjucks.ConfigureOptions = {
     autoescape: true,
     express: app,
-    // Don't cache in development mode so we can make changes to templates without restarting the server
-    noCache: isDevelopment,
+    // Don't cache so we can make changes to templates without restarting the server
+    noCache: true,
   };
   const viewPaths = [viewsPath, "node_modules/govuk-frontend/"];
   nunjucks.configure(viewPaths, configureOptions);
